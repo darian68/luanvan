ad6226ab7294d7dba5b40144107fba97b83b0e04
Darian <dai.n.ung@gmail.com>
2015-04-22 17:01:45 +0700

    Adjust data

---
+++ code/Main/Data/LoadAmcFile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Main.Data
+{
+    class LoadAmcFile
+    {
+        public string[] pointnames;
+        public List<double[]> vectorlist;
+        public LoadAmcFile(string path, string[] pointnames)
+        {
+            this.pointnames = pointnames;
+            this.vectorlist = new List<double[]>();
+            var lines = File.ReadAllLines(path);
+            List<double> vector = new List<double>();
+
+            foreach (var line in lines)
+            {
+                String[] temp = line.Split(' ');
+                for (int n = 0; n < this.pointnames.Length; n++)
+                //Lặp trong mảng pointnames[]
+                {
+                    if (line.StartsWith(this.pointnames[n]))
+                    {
+                        int length = temp.Length;
+                        for (int j = 1; j < length; j++)
+                            vector.Add(double.Parse(temp[j]));
+                        if (n == this.pointnames.Length - 1)
+                        {
+                            vectorlist.Add(vector.ToArray());
+                            vector = new List<double>();
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
--- code/Main/MainForm.cs
+++ code/Main/MainForm.cs
@@ -11,6 +11,7 @@ using Main.Data;
 using Accord.Statistics.Kernels;
 using Accord.MachineLearning.VectorMachines;
 using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.Math;
 
 namespace Main
 {
@@ -24,12 +25,13 @@ namespace Main
         private void btnSVM_Click(object sender, EventArgs e)
         {
             txtOutput.Text = "";
-            string pathRun = "D:\\acclaim\\training\\run";
-            string pathWalk = "D:\\acclaim\\training\\walk";
-            string pathJump = "D:\\acclaim\\training\\jump";
-            string pathDance = "D:\\acclaim\\training\\dance";
+            string pathRun = "C:\\acclaim\\training\\run";
+            string pathWalk = "C:\\acclaim\\training\\walk";
+            string pathJump = "C:\\acclaim\\training\\jump";
+            string pathDance = "C:\\acclaim\\training\\dance";
             // All bones
-            string[] boneNames = new string[] { "root", "lhipjoint", "rhipjoint", "lowerback", "lhipjoint", "lfemur", "lfemur", "ltibia", "ltibia", "lfoot", "lfoot", "ltoes", "rhipjoint", "rfemur", "rfemur", "rtibia", "rtibia", "rfoot", "rfoot", "rtoes", "lowerback", "upperback", "upperback", "thorax", "thorax", "lowerneck", "lclavicle", "rclavicle", "lowerneck", "upperneck", "upperneck", "head", "lclavicle", "lhumerus", "lhumerus", "lradius", "lradius", "lwrist", "lwrist", "lhand", "lthumb", "lhand", "lfingers", "rclavicle", "rhumerus", "rhumerus", "rradius", "rradius", "rwrist", "rwrist", "rhand", "rthumb", "rhand", "rfingers" };
+            //string[] boneNames = new string[] {"lhipjoint", "rhipjoint", "lowerback", "lhipjoint", "lfemur", "lfemur", "ltibia", "ltibia", "lfoot", "lfoot", "ltoes", "rhipjoint", "rfemur", "rfemur", "rtibia", "rtibia", "rfoot", "rfoot", "rtoes", "lowerback", "upperback", "upperback", "thorax", "thorax", "lowerneck", "lclavicle", "rclavicle", "lowerneck", "upperneck", "upperneck", "head", "lclavicle", "lhumerus", "lhumerus", "lradius", "lradius", "lwrist", "lwrist", "lhand", "lthumb", "lhand", "lfingers", "rclavicle", "rhumerus", "rhumerus", "rradius", "rradius", "rwrist", "rwrist", "rhand", "rthumb", "rhand", "rfingers" };
+            string[] boneNames = new string[] { "lhipjoint", "rhipjoint", "rfemur", "lfemur", "rtibia", "ltibia", "rfoot", "head", "rhumerus", "lhumerus", "rhand", "lhand", "lfoot"};
             //
             LoadAmcFolder amcRun = new LoadAmcFolder(pathRun, boneNames);
             LoadAmcFolder amcWalk = new LoadAmcFolder(pathWalk, boneNames);
@@ -41,38 +43,18 @@ namespace Main
             double[][] danceInput = amcDance.vectorlist.ToArray();
             List<double[]> inputs = new List<double[]>();
             List<int> outputs = new List<int>();
-            // Get input from run
-            int size = runInput.Count();
-            for (int s = 0; s < size; s++)
-            {
-                inputs.Add(runInput[s]);
-                outputs.Add(Activity.RUN);
-            }
-            // Get input from walk
-            size = walkInput.Count();
-            for (int s = 0; s < size; s++)
-            {
-                inputs.Add(walkInput[s]);
-                outputs.Add(Activity.WALK);
-            }
+            inputs.Add(Matrix.Concatenate(runInput));
+            outputs.Add(Activity.RUN);
+            inputs.Add(Matrix.Concatenate(walkInput));
+            outputs.Add(Activity.WALK);
             /*
-            // Get input from jump
-            size = jumpInput.Count();
-            for (int s = 0; s < size; s++)
-            {
-                inputs.Add(jumpInput[s]);
-                outputs.Add(Activity.JUMP);
-            }
-            // Get input from dance
-            size = danceInput.Count();
-            for (int s = 0; s < size; s++)
-            {
-                inputs.Add(danceInput[s]);
-                outputs.Add(Activity.DANCE);
-            }
+            inputs.Add(Matrix.Concatenate(jumpInput));
+            outputs.Add(Activity.JUMP);
+            inputs.Add(Matrix.Concatenate(danceInput));
+            outputs.Add(Activity.DANCE);
              */
             // Create a new Linear kernel
-            IKernel kernel = new DynamicTimeWarping(1);
+            IKernel kernel = new DynamicTimeWarping(62);
             // Create a new Multi-class Support Vector Machine with 1 input,
             //  using the linear kernel and for four disjoint classes.
             var machine = new MulticlassSupportVectorMachine(0, kernel, 2);
@@ -85,6 +67,26 @@ namespace Main
             // Run the learning algorithm
             double error = teacher.Run();
             txtOutput.Text = "Finish training!";
+            string pathPatternRun = "C:\\acclaim\\pattern\\09_01.amc"; //run 0
+            LoadAmcFile runFile = new LoadAmcFile(pathPatternRun, boneNames);
+            int result = machine.Compute(Matrix.Concatenate(runFile.vectorlist.ToArray()));
+            txtOutput.Text += "\nResult Class: " + result;
+            pathPatternRun = "C:\\acclaim\\pattern\\07_01.amc";// walk 1
+            runFile = new LoadAmcFile(pathPatternRun, boneNames);
+            result = machine.Compute(Matrix.Concatenate(runFile.vectorlist.ToArray()));
+            txtOutput.Text += "\nResult Class: " + result;
+            pathPatternRun = "C:\\acclaim\\pattern\\16_55.amc"; // run 0
+            runFile = new LoadAmcFile(pathPatternRun, boneNames);
+            result = machine.Compute(Matrix.Concatenate(runFile.vectorlist.ToArray()));
+            txtOutput.Text += "\nResult Class: " + result;
+            pathPatternRun = "C:\\acclaim\\pattern\\07_03.amc"; // walk 1
+            runFile = new LoadAmcFile(pathPatternRun, boneNames);
+            result = machine.Compute(Matrix.Concatenate(runFile.vectorlist.ToArray()));
+            txtOutput.Text += "\nResult Class: " + result;
+            pathPatternRun = "C:\\acclaim\\pattern\\16_35.amc"; // run 0
+            runFile = new LoadAmcFile(pathPatternRun, boneNames);
+            result = machine.Compute(Matrix.Concatenate(runFile.vectorlist.ToArray()));
+            txtOutput.Text += "\nResult Class: " + result;
         }
     }
 }
