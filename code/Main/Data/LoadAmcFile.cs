9c510c42c2ca6acfcb1194208246811fe079b733
Darian <dai.n.ung@gmail.com>
2015-04-23 14:18:36 +0700

    Adjust LoadAmcFile and Folder

--- code/Main/Data/LoadAmcFile.cs
+++ code/Main/Data/LoadAmcFile.cs
@@ -9,19 +9,29 @@ namespace Main.Data
 {
     class LoadAmcFile
     {
-        public List<double> data;
-        public LoadAmcFile(string fileName, string[] pointnames)
+        private string fileName;
+        private string[] boneNames;
+        public LoadAmcFile(string fileName, string[] boneNames)
         {
-            int numBones = pointnames.Length;
-            string[] lines = File.ReadAllLines(fileName);
-            data = new List<double>();
+            this.fileName = fileName;
+            this.boneNames = boneNames;
+        }
+        /*
+         * This method return an array for all dof values
+         */
+        public double[] readDataAsVetor()
+        {
+            List<double> data = new List<double>();
+            int numBones = boneNames.Length;
+            string[] lines;
             String[] temp;
+            lines = File.ReadAllLines(fileName);
             foreach (var line in lines)
             {
                 temp = line.Split(' ');
                 for (int n = 0; n < numBones; n++)
                 {
-                    if (line.StartsWith(pointnames[n]))
+                    if (line.StartsWith(boneNames[n]))
                     {
                         int length = temp.Length;
                         int j;
@@ -37,7 +47,66 @@ namespace Main.Data
                         break;
                     }
                 } // end for bones
+            }
+            return data.ToArray();
+        }
+
+        /*
+         * This method return 2 dimention array
+         * Each elemnt contain data of a frame
+         */
+        public double[][] readDataAs2DVetor()
+        {
+            List<double[]> data = new List<double[]>();
+            List<double> frame;
+            int numBones = boneNames.Length;
+            string[] lines;
+            String[] temp;
+            lines = File.ReadAllLines(fileName);
+            frame = new List<double>();
+            int lineIndex = 0;
+            foreach (string line in lines)
+            {
+                temp = line.Split(' ');
+                for (int n = 0; n < numBones; n++)
+                {
+                    if (line.StartsWith(boneNames[n]))
+                    {
+                        int length = temp.Length;
+                        int j;
+                        for (j = 1; j < length; j++)
+                        {
+                            frame.Add(double.Parse(temp[j]));
+                        }
+                        // if dof has less then 3 value, set 0 for the rest
+                        for (int k = j; k < 4; k++)
+                        {
+                            frame.Add(0.00);
+                        }
+                        break;
+                    }
+                } // end for bones
+                // Check frame number in the file
+                if (temp.Length == 1)
+                {
+                    if (frame.Count > 0)
+                    {
+                        data.Add(frame.ToArray());
+                    }
+                    frame = new List<double>();
+                }
+                // Add last frame
+                if (lineIndex == lines.Length - 1)
+                {
+                    if (frame.Count > 0)
+                    {
+                        data.Add(frame.ToArray());
+                    }
+                    frame = new List<double>();
+                }
+                lineIndex++;
             }// end for lines
+            return data.ToArray();
         }
     }
 }
--- code/Main/Data/LoadAmcFolder.cs
+++ code/Main/Data/LoadAmcFolder.cs
@@ -23,41 +23,15 @@ namespace Main.Data
         public double[][] readDataAs2DVetor() {
             string[] fileList = Directory.GetFiles(this.path, "*.amc");
             List<double[]> data = new List<double[]>();
-            List<double> dof;
             int numFiles = fileList.Length;
             int numBones = boneNames.Length;
             string fileName;
-            string[] lines;
-            String[] temp;
+            LoadAmcFile amcFile;
             for (int f = 0; f < numFiles; f++)
             {
                 fileName = fileList[f];
-                lines = File.ReadAllLines(fileName);
-                dof = new List<double>();
-                foreach (var line in lines)
-                {
-                    temp = line.Split(' ');
-                    for (int n = 0; n < numBones; n++) 
-                    {
-                        if (line.StartsWith(boneNames[n]))
-                        {
-                            int length = temp.Length;
-                            int j;
-                            for (j = 1; j < length; j++)
-                            {
-                                dof.Add(double.Parse(temp[j]));
-                            }
-                            // if dof has less then 3 value, set 0 for the rest
-                            for (int k = j; k < 4; k++)
-                            {
-                                dof.Add(0.00);
-                            }
-                            break;
-                        }
-                    } // end for bones
-                }// end for lines
-                // Add data of a file to the list
-                data.Add(dof.ToArray());
+                amcFile = new LoadAmcFile(fileName, this.boneNames);
+                data.Add(amcFile.readDataAsVetor());
             }
             return data.ToArray();
         }
@@ -71,63 +45,15 @@ namespace Main.Data
         {
             List<double[][]> data = new List<double[][]>();
             string[] fileList = Directory.GetFiles(this.path, "*.amc");
-            List<double[]> dof;
-            List<double> frame;
             int numFiles = fileList.Length;
             int numBones = boneNames.Length;
             string fileName;
-            string[] lines;
-            String[] temp;
+            LoadAmcFile amcFile;
             for (int f = 0; f < numFiles; f++)
             {
                 fileName = fileList[f];
-                lines = File.ReadAllLines(fileName);
-                dof = new List<double[]>();
-                frame = new List<double>();
-                int lineIndex = 0;
-                foreach (string line in lines)
-                {
-                    temp = line.Split(' ');
-                    for (int n = 0; n < numBones; n++)
-                    {
-                        if (line.StartsWith(boneNames[n]))
-                        {
-                            int length = temp.Length;
-                            int j;
-                            for (j = 1; j < length; j++)
-                            {
-                                frame.Add(double.Parse(temp[j]));
-                            }
-                            // if dof has less then 3 value, set 0 for the rest
-                            for (int k = j; k < 4; k++)
-                            {
-                                frame.Add(0.00);
-                            }
-                            break;
-                        }
-                    } // end for bones
-                    // Check frame number in the file
-                    if (temp.Length == 1)
-                    {
-                        if (frame.Count > 0)
-                        {
-                            dof.Add(frame.ToArray());
-                        }
-                        frame = new List<double>();
-                    }
-                    // Add last frame
-                    if (lineIndex == lines.Length - 1)
-                    {
-                        if (frame.Count > 0)
-                        {
-                            dof.Add(frame.ToArray());
-                        }
-                        frame = new List<double>();
-                    }
-                    lineIndex++;
-                }// end for lines
-                // Add data of a file to the list
-                data.Add(dof.ToArray());
+                amcFile = new LoadAmcFile(fileName, this.boneNames);
+                data.Add(amcFile.readDataAs2DVetor());
             }
             return data.ToArray();
         }
